c7b0f4e6b8f0a8aa3989c2ccaff55de2f78e8dee
Laurine <laurine.lafontaine@outlook.fr>
2020-03-27 09:15:15 +0100

    Started Spring boot project

---
+++ fakedetection/src/main/java/com/afp/medialab/weverify/fakedetection/FakedetectionApplication.java
@@ -0,0 +1,13 @@
+package com.afp.medialab.weverify.fakedetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakedetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakedetectionApplication.class, args);
+	}
+
+}
---
+++ fakedetection/src/test/java/com/afp/medialab/weverify/fakedetection/FakedetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afp.medialab.weverify.fakedetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakedetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
